bd951463c35ddf20036b2c1f096aabd9daefe021
sarapuertas <saritapuertas@hotmail.com>
2022-03-10 18:48:40 +0100

    css

--- src/components/PlacesCard/PlacesCard.jsx
+++ src/components/PlacesCard/PlacesCard.jsx
@@ -1,3 +1,4 @@
+import '../PlacesCard/PlacesCard.css'
 import { Card, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
@@ -6,16 +7,18 @@ const PlacesCard = ({ favPlace }) => {
     const { name, type, image } = favPlace
 
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={image} />
-            <Card.Body>
-                <Card.Title> {name}</Card.Title>
-                <Card.Text>{type}</Card.Text>
-                <Link to={`/detalles/${favPlace?._id}`}>
-                    <Button variant="primary">Detalles</Button>
-                </Link>
-            </Card.Body>
-        </Card>
+        <div className='favplace-card'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={image} />
+                <Card.Body>
+                    <Card.Title> {name}</Card.Title>
+                    <Card.Text>{type}</Card.Text>
+                    <Link to={`/detalles/${favPlace?._id}`}>
+                        <Button variant="primary">Detalles</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
+        </div >
     )
 }
 
